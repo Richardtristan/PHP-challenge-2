5eaafe443462018e6cb49b323dc2888827f64d6d
tristan <richard.tristan.93@gmail.com>
2021-08-23 15:20:39 +0200

    commit update

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
+require 'vendor/autoload.php';
+
+include 'class/Router.php';
 
 $request = $_SERVER['REQUEST_URI'];
+$router = new Router($request);
 
-switch ($request) {
-    case '/' :
-        case '/home' :
-        require __DIR__ . '/assets/php/home.php';
-        break;
-    case '/test' :
-        require __DIR__ . '/assets/php/test.php';
-        break;
-    default:
-        http_response_code(404);
-        require __DIR__ . '/assets/php/404.php';
-        break;
-}
+$router->get('/', 'src/php/home');
+$router->get('test', 'src/php/test');
