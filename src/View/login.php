b84a370e8289868c966dd7d3616e0283722d785d
Elisandre <elisandre.laenens@gmail.com>
2021-08-25 11:33:11 +0200

    html login

---
+++ src/View/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <!-- insert header here -->
+    <h1>Welcome to the COGIP</h1>
+    <h2>Bienvenue dans l'espace de connexion de la COGIP</h2>
+    <form action="" method='POST'>
+        <label for="username">Votre identifiant</label>
+        <input type="text" name="Login" id="username" class='username'>
+        <label for="mdp">Votre mot de passe</label>
+        <input type="password" name="Password" id="mdp" class='mdp'>
+        <input type="submit" value="Submit">
+    </form>
+    <!-- insert footer here -->
+</body>
+</html>
