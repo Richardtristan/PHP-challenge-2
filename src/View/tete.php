eecbd835fee63691181d954919f7cfa7f65dfb0d
martin trufley <pro.trufley@gmail.com>
2021-08-25 11:48:47 +0200

    header footer

---
+++ src/View/tete.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COGIP</title>
+</head>
+<body>
+
+<nav class="navbar">
+<p>Vous êtes connecté en tant que :</p>
+    <ol>
+        <li class="home"><a href="#">Home</a></li>
+        <li class="invoices"><a href="#">Invoices</a></li>
+        <li class="compagnies"><a href="#">Compagnies</a></li>
+        <li class="contacts"><a href="#">Contacts</a></li>
+        <li class="admin"><a href="#">Admin</a></li>  
+    </ol>
+    <input class="Logout"
+       type="button"
+       value="Log out">
+
+</nav>
+</body>
+</html>
+
