64c3c78c2a7ef0d90fade82cd638dea7aaf46c0c
tristan <richard.tristan.93@gmail.com>
2021-08-25 16:02:38 +0200

    commit update

--- src/View/php/header.php
+++ src/View/php/header.php
@@ -1,24 +1,29 @@
+<?php
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="../../../public/assets/js/main.js" </script>
+    <script type="text/javascript" src="../../../public/assets/js/main.js"></script>
     <link rel="stylesheet" type="text/css" href="../../../public/assets/css/main.css">
     <title>COGIP</title>
 </head>
 <body>
 
 <nav class="navbar">
-    <p>Vous êtes connecté en tant que :</p>
+    <p>Vous êtes connecté en tant que :<?php $_SESSION['Login'] ?> </p>
     <ol>
         <li class="home"><a href="#">Home</a></li>
         <li class="invoices"><a href="#">Invoices</a></li>
         <li class="compagnies"><a href="#">Compagnies</a></li>
         <li class="contacts"><a href="#">Contacts</a></li>
-        <li class="admin"><a href="#">Admin</a></li>
     </ol>
+    
     <input class="Logout"
            type="button"
            value="Log out">
--- src/View/php/welcome.php
+++ src/View/php/welcome.php
@@ -3,15 +3,9 @@ session_start();
 if (!isset($_SESSION['username'])){
     header('location: /connect');
 }
+require 'header.php';
 ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Welcome</title>
-    </head>
+
     <body>
     <!-- Insert header here -->
     <h1>Welcome to the COGIP</h1>
@@ -61,5 +55,4 @@ if (!isset($_SESSION['username'])){
             </tr>
         </table>
     </div>
-    </body>
-    </html><?php
+    <?php require 'footer.php';
