51dc528d36d098bf4d29209478e39ddd9fe8b719
Elisandre <elisandre.laenens@gmail.com>
2021-08-25 11:40:57 +0200

    welcome page

--- src/View/welcome.php
+++ src/View/welcome.php
@@ -10,18 +10,50 @@
     <!-- Insert header here -->
     <h1>Welcome to the COGIP</h1>
     <p>Bonjour !</p>
-    <label for="invoice">Last invoice :</label>
-    <table id='invoice'>
-        <tr>
-            <th>Invoice number</th>
-            <th>Dates</th>
-            <th>Company</th>
-        </tr>
-        <tr>
-            <td>xInvoice</td>
-            <td>xDate</td>
-            <td>xCompany</td>
-        </tr>
-    </table>
+    <div>
+        <label for="invoice">Last invoice :</label>
+        <table id='invoice'>
+            <tr>
+                <th>Invoice number</th>
+                <th>Dates</th>
+                <th>Company</th>
+            </tr>
+            <tr>
+                <td>xInvoice</td>
+                <td>xDate</td>
+                <td>xCompany</td>
+            </tr>
+        </table>
+        <label for="contact">Last contacts :</label>
+        <table id='contact'>
+            <tr>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>e-mail</th>
+                <th>Company</th>
+            </tr>
+            <tr>
+                <td>xName</td>
+                <td>xPhone</td>
+                <td>xe-mail</td>
+                <td>xCompany</td>
+            </tr>
+        </table>
+        <label for="companies">Last companies :</label>
+        <table id='companies'>
+            <tr>
+                <th>Name</th>
+                <th>TVA</th>
+                <th>Country</th>
+                <th>Type</th>
+            </tr>
+            <tr>
+                <td>xName</td>
+                <td>xTVA</td>
+                <td>xCountry</td>
+                <td>xType</td>
+            </tr>
+        </table>
+    </div>
 </body>
 </html>
