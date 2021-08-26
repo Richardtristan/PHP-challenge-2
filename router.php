857d647b4c2e972abd316588141147e1acc0f609
tristan <richard.tristan.93@gmail.com>
2021-08-26 13:33:33 +0200

    Revert "commit update"
    This reverts commit 6933b592e0337b6cccd7cc28f96d850cff08c697.

--- router.php
+++ router.php
@@ -19,7 +19,7 @@ function delete($route, $path_to_include){
 }
 function any($route, $path_to_include){ route($route, $path_to_include); }
 function route($route, $path_to_include){
-  $ROOT = __DIR__;
+  $ROOT = $_SERVER['DOCUMENT_ROOT'];
   if($route == "/404"){
     include_once("$ROOT/$path_to_include");
     exit();
