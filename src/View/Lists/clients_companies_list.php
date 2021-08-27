2736de01e4f5f200496f2101a269dc684606d8a0
tristan <richard.tristan.93@gmail.com>
2021-08-27 16:07:10 +0200

    update

---
+++ src/View/Lists/clients_companies_list.php
@@ -0,0 +1,22 @@
+<?php require __DIR__.'/../surround/header.php' ?>
+<div>
+    <label for="companies">List of clients companies :</label>
+    <table id='compagnies'>
+        <tr>
+            <th>Name</th>
+            <th>VAT</th>
+            <th>Country</th>
+        </tr>
+        <?php foreach ($_SESSION['companies_all'] as $companie) {
+            ?>
+            <tr>
+                <td><?php echo $companie["name"] ?></td>
+                <td><?php echo $companie["VAT_number"] ?></td>
+                <td><?php echo $companie["country"] ?></td>
+            </tr>
+            <?php
+        }
+        ?>
+    </table>
+</div>
+<?php require __DIR__.'/../surround/footer.php' ?>
