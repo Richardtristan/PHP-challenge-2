20a133b19be5035972dd8ee978a4563d9a4e6f72
Elis <elisandre.laenens@gmail.com>
2021-09-01 09:35:13 +0200

    footer svg

--- src/View/surround/footer.php
+++ src/View/surround/footer.php
@@ -1,8 +1,8 @@
 <?php
 ?>
 
-<footer class="footer">
-    <div class="content has-text-centered">
+<footer>
+    <div class="footer">
         <?php if (isset($_SESSION['username'])){?>
         <p class='connexion'>Vous êtes connecté en tant que : <?php echo $_SESSION['username'] ?> </p><br><?php }?>
         <p class='slogan'>Vive la COGIP ! </p>
