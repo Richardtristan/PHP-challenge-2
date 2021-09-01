f9bfec26991b9566fa00d6f1b8b2cd1dcd730d5f
tristan <richard.tristan.93@gmail.com>
2021-09-01 14:15:24 +0200

    update

--- src/Controller/UsersListController.php
+++ src/Controller/UsersListController.php
@@ -2,12 +2,13 @@
 
 use App\Model\UsersList;
 
-
+if ($_SESSION['grade'] === 'Admin'){
 $users = new \App\Model\UsersList();
 $users->setUsersList();
 
 $users = $users->getUsersList()->fetchAll();
 require __DIR__.'/../view/admin/admin_dashboard.php';
+}else{header('location: /home');}
 
 //$_SESSION['invoices_all'] = $invoicesAll->getAllList()->fetchAll();
 
