d231e76b002e2d84fd88af5c466a878a357a3d07
Elis <elisandre.laenens@gmail.com>
2021-09-01 11:27:08 +0200

    footer done

--- src/View/surround/footer.php
+++ src/View/surround/footer.php
@@ -4,7 +4,7 @@
 <footer>
     <div class="footer">
         <?php if (isset($_SESSION['username'])){?>
-        <p class='connexion'>Vous êtes connecté en tant que : <?php echo $_SESSION['username'] ?> </p><br><?php }?>
+        <p class='connexion'>Vous êtes connecté en tant que : <br> <?php echo $_SESSION['username'] ?> </p><?php }?>
         <p class='slogan'>Vive la COGIP ! </p>
     </div>
 </footer>
