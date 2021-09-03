6e8331786c3ad294fa7471d8118166f1934924d9
Elisandre <elisandre.laenens@gmail.com>
2021-09-03 08:47:09 +0200

    cl

---
+++ src/View/Lists/contact_list.php
@@ -0,0 +1,23 @@
+<?php require __DIR__.'/../surround/header.php';
+?>
+
+    <div>
+    <table id='contact_list'>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Company</th>
+        </tr>
+        <?php foreach ($contacts as $contact) {
+            ?>
+            <tr>
+                <td><a href="/contact_details/id/<?php echo $contact['id']?>"><?php echo $contact["firstname"] . ' ' . $contact["lastname"]?></a></td>
+                <td><?php echo $contact["email"] ?></td>
+                <td><?php echo $contact["name"] ?></td>
+            </tr>
+            <?php
+        }
+        ?>
+    </table>
+</div>
+<?php require __DIR__.'/../surround/footer.php' ?>
